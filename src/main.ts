bb0f70fbd3ea2f5ed795aa3e7537144f61a8bb3d
Amme <anne-sophie.nehls@t-online.de>
2024-08-19 15:37:57 +0200

    Credite Card output im THML wurde erstellt, aber noch nicht mit den Values verbunden

--- src/main.ts
+++ src/main.ts
@@ -31,3 +31,23 @@ formElement.addEvebtlistener("click", (event: Event) => {
   creditCards.push(newCreditCard);
 });
 
+//!_____ Ab hier Create Credite Card in HTML_____ -> Output
+//# Div Card 
+const newCardDiv = document.createElement("div") as HTMLDivElement;
+newCardDiv.className = "creditCard";
+
+const outputFeld = document.getElementById("creditCardOutput") as HTMLElement
+outputFeld.appendChild(newCardDiv)
+//# Card number im div
+const cardNumberOutput = document.createElement("b") as HTMLParagraphElement
+cardNumberOutput.className = "NumberOutput"
+newCardDiv.appendChild(cardNumberOutput)
+//# Card holder im div
+const cardHolderOutput = document.createElement("p") as HTMLParagraphElement
+cardHolderOutput.className = "HolderOutput"
+newCardDiv.appendChild(cardHolderOutput)
+//# Card Date im div
+const cardDateOutput = document.createElement("p") as HTMLParagraphElement
+cardDateOutput.className = "DateOutput"
+newCardDiv.appendChild(cardDateOutput)
+//!______Ende Create Credit Card in HTML_______ -> Output
