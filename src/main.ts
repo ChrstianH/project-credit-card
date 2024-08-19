49e1c52a0bed1add9784473a3b04341335a3f9f6
SteveDwumah <Steve-marvin-dwumah@hotmail.de>
2024-08-19 14:42:34 +0200

    validate add

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,7 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+function validateCardNumber(cardNumber: string): boolean {
+    return cardNumber.length === 16 && !isNaN(Number(cardNumber));
+}
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+function validateCvvNumber(cvvNumber: string): boolean {
+  return cvvNumber.length === 3 && !isNaN(Number(cvvNumber))
+}
