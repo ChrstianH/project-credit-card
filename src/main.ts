3ce16f1e45113c766b09cac710f1303b35ef5109
hensc <henschel_christian@web.de>
2024-08-19 14:42:26 +0200

    more Logic added

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import CreditCard from "./classes/CreditCard";
 
+const creditCards: CreditCard[] = [];
+
+const formElement = document.getElementById("cardInfoForm") as HTMLFormElement;
 const cardNumber = document.getElementById("cardNumber") as HTMLInputElement;
 const cardHolder = document.getElementById("cardHolder") as HTMLInputElement;
 const expirationDate = document.getElementById(
@@ -7,3 +10,14 @@ const expirationDate = document.getElementById(
 ) as HTMLInputElement;
 expirationDate.setAttribute("min", "2024-08");
 const cvv = document.getElementById("cvv") as HTMLInputElement;
+
+formElement.addEvebtlistener("click", (event: Event) => {
+  event.preventDefault();
+  const newCreditCard: CreditCard = new CreditCard(
+    cardNumber.value,
+    cardHolder.value,
+    expirationDate.value,
+    cvv.value
+  );
+  creditCards.push(newCreditCard);
+});
