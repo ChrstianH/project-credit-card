baac0521ab99cb60727839f485f1a1c5ac22f8f4
hensc <henschel_christian@web.de>
2024-08-19 14:30:36 +0200

    Logic elements added

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
 import CreditCard from "./classes/CreditCard";
+
+const cardNumber = document.getElementById("cardNumber") as HTMLInputElement;
+const cardHolder = document.getElementById("cardHolder") as HTMLInputElement;
+const expirationDate = document.getElementById(
+  "expirationDate"
+) as HTMLInputElement;
+expirationDate.setAttribute("min", "2024-08");
+const cvv = document.getElementById("cvv") as HTMLInputElement;
